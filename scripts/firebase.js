22a923a7d28418f6b10c8f0335f6c6af4f1da62d
Angela Yu <ayu107@my.bcit.ca>
2023-03-11 19:52:14 -0800

    firestore

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,36 +1,15 @@
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-var uiConfig = {
-    callbacks: {
-      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        return true;
-      },
-      uiShown: function() {
-        // The widget is rendered.
-        // Hide the loader.
-        document.getElementById('loader').style.display = 'none';
-      }
-    },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: "main.html",
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    //   firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    //   firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    //   firebase.auth.PhoneAuthProvider.PROVIDER_ID
-    ],
-    // Terms of service url.
-    tosUrl: '<your-tos-url>',
-    // Privacy policy url.
-    privacyPolicyUrl: '<your-privacy-policy-url>'
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDr4gXS3vcvg4x2byr7hiIlDGBXHVjhRd4",
+    authDomain: "motivatemilo.firebaseapp.com",
+    projectId: "motivatemilo",
+    storageBucket: "motivatemilo.appspot.com",
+    messagingSenderId: "250888214200",
+    appId: "1:250888214200:web:05dcf99d3038d8101c864d"
   };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = firebase.firestore();
 
-  ui.start('#firebaseui-auth-container', uiConfig);
+
