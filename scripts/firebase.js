2c393452116d4a31ca73789ecab63e07c20ab33a
Victor Fung <98victorfung@gmail.com>
2023-03-11 20:08:13 -0800

    fixed firebase.js so it works

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -9,7 +9,5 @@ const firebaseConfig = {
   };
   
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  const app = firebase.initializeApp(firebaseConfig);
   const db = firebase.firestore();
-
-
