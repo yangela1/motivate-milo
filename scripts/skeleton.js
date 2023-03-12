b6a19d7deda72d190cccad485875b33e4d731c77
Victor Fung <98victorfung@gmail.com>
2023-03-11 21:34:41 -0800

    added quote of the day to main.html

--- scripts/skeleton.js
+++
@@ -1,8 +0,0 @@
-//---------------------------------------------------
-// This function loads the parts of your skeleton 
-// (navbar, footer, and other things) into html doc. 
-//---------------------------------------------------
-function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./text/nav.html'));
-}
-loadSkeleton();  //invoke the function
